9099d1155698e03c8955de4453c9187b354081d4
HappyOgre <danielfuchs48@gmail.com>
2024-05-23 23:33:47 +0200

    Create index.js

---
+++ index.js
@@ -0,0 +1,33 @@
+import sqlite3 from 'sqlite3';
+let sql = `SELECT name FROM movesets`;
+
+//connect to DB
+let db = new sqlite3.Database("dsa.db", sqlite3.OPEN_READWRITE, (err) => {
+    if (err) {
+        console.log(err.message);
+    } else {
+    console.log('Connected to the database.')
+    }
+})
+
+
+db.serialize(() => {
+    db.run(sql, (err) => {
+        if (err) {
+            console.log(err.message);
+        }
+        
+    })
+});
+/*
+db.all(sql, [], (err, rows) => {
+    if (err) {
+        console.log(err.message);
+    }
+    rows.forEach((row) => {
+        if (err) {
+            console.log(err.message);
+        };
+    });
+});
+*/
